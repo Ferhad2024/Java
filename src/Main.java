a804e2274d0cddff39ba247f635d88d8c02f5a39
ferhad <ferhadismayilov2024@gmail.com>
2025-01-01 01:02:51 +0400

    master2 brachina deyisdim

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Repository yaratdim");
         System.out.println("Hello, World!");
+        System.out.println( "master2 brachina deyisdim");
     }
 }
