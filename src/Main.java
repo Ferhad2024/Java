ebbe9e2bcf8385683837f394fab398a65efafcc5
ferhad <ferhadismayilov2024@gmail.com>
2025-01-01 10:18:05 +0400

    add new line

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         System.out.println("Hello, World!");
         System.out.println("Repository yaratdim");
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Wrld!");
+        System.out.println("fix branchi yaratdim");
     }
 }
